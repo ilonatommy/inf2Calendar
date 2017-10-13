27e36f9a9a845d3a35fdb0cc9e72779efa54616a
ilonatommy <ili.ili@interia.pl>
2017-10-13 09:28:44 +0200

    Data.hpp

---
+++ Date.hpp
@@ -0,0 +1,29 @@
+/*
+ * Date.hpp
+ *
+ *  Created on: 13 Oct 2017
+ *      Author: ilona
+ */
+
+#ifndef DATE_HPP_
+#define DATE_HPP_
+
+class Date{
+	int year;
+	int month;
+	int day;
+
+	Date(year = 1, month = 1, day = 1);
+	~Date();
+
+private:
+	friend operator <<();
+	friend operator >>();
+	Date operator +();
+	Date operator -();
+};
+
+
+
+
+#endif /* DATE_HPP_ */
