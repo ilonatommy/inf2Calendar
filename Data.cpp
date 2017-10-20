80f2c3cbad98d1d1b78d307d6edf68eca32c6604
ilonatommy <32700855+ilonatommy@users.noreply.github.com>
2017-10-20 10:25:41 +0200

    korekty

--- Data.cpp
+++ Data.cpp
@@ -1,9 +1,6 @@
-
 #include "Date.hpp"
 #include <iostream>
 
-class Date{
-	
 const std::array<const int, 12> monthsLength = {
     		31, // Jan
     		28, // Feb
@@ -19,42 +16,53 @@ const std::array<const int, 12> monthsLength = {
     		31  // Dec
     	};
 	
-	Date::Date(int _year = 1970, int _month = 1, int _day = 1)
-	{
-		year=_year;
-		month=_month;
-		day=_day;
-	};
+Date::Date(int _year = 1970, int _month = 1, int _day = 1)
+{
+	year=_year;
+	month=_month;
+	day=_day;
+};
 
-	int dateToDays(const Date& date)
-	{
-		int years = date.year - 1970;
-		int months = date.month - 1;
-		int days = date.day; //bo 1970.01.01 daje jeden dzień - przyjmuję to za wartość początkową kalendarza
-		days += 365 * years;
-		days += monthsLength[date.month - 1] * months;
+int Date::dateToDays(const Date& date)
+{
+	int years = date.year - 1970;
+	int months = date.month - 1;
+	int days = date.day; //bo 1970.01.01 daje jeden dzień - przyjmuję to za wartość początkową kalendarza
+	days += 365 * years;
+	days += monthsLength[date.month - 1] * months;
 		
-		return days;
-	}
-	
-	Date& daysToDate(int days)
-	{
-		int years = days / 365;
-		days -= 365 * years;
-		int months;
-		for(months = 1; months <= 12; months++)
-		{
-			if(days - monthsLength[months] > 0)
-				days -= monthsLength[months];
-			else break;
-		}
-		Date date;
-		date.year = years + 1970;
-		date.month = months;
-		date.day = days;
-		
-		return &date;
+	return days;
 }
+	
+Date& Date::daysToDate(int days)
+{
+	int years = days / 365;
+	days -= 365 * years;
+	int months;
+	for(months = 1; months <= 12; months++)
+	{
+		if(days - monthsLength[months] > 0)
+			days -= monthsLength[months];
+		else break;
+	}
+	Date date;
+	date.year = years + 1970;
+	date.month = months;
+	date.day = days;
+		
+	return &date;
+}
+	
+Date Date::operator +(const Date& D)
+{
+	int days1 = dateToDays(this);
+	int days2 = dateToDays(D);
+	Date sum;
+	sum = daysToDate(days1+days2);
+		
+	return sum;
+}
+
 
 Date operator +(int& n, const Date& D)
 {
