04011951f2799aad8f667c285c3de763a641e2a4
ilonatommy <32700855+ilonatommy@users.noreply.github.com>
2017-10-20 09:47:56 +0200

    wrzucenie monthLength

--- Data.cpp
+++ Data.cpp
@@ -2,7 +2,22 @@
 #include <iostream>
 
 class Date{
-
+	
+const std::array<const int, 12> monthsLength = {
+    		31, // Jan
+    		28, // Feb
+    		31, // Mar
+    		30, // Apr
+    		31, // May
+    		30, // Jun
+    		31, // Jul
+    		31, // Aug
+    		30, // Sep
+    		31, // Oct
+    		30, // Nov
+    		31  // Dec
+    	};
+	
 	Date::Date(int _year = 1970, int _month = 1, int _day = 1)
 	{
 		year=_year;
@@ -21,6 +36,7 @@ class Date{
 		return days;
 	}
 
+	
 
 }
 
