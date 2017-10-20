f8a043b48a06de09263e3bcaeb1346087c0486f7
ilonatommy <32700855+ilonatommy@users.noreply.github.com>
2017-10-20 09:40:32 +0200

    dateToDays

--- Data.cpp
+++ Data.cpp
@@ -11,11 +11,15 @@ class Date{
 	};
 
 	static int dateToDays(const Date& date)
-		{
-			int years = date.year - 1970;
-			int
-			return date;
-		}
+	{
+		int years = date.year - 1970;
+		int months = date.month - 1;
+		int days = date.day - 1;
+		days += 365 * years;
+		days += monthsLength[date.month - 1] * months;
+		
+		return days;
+	}
 
 
 }
