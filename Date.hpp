f5d737b27dba5e884d92f8bd2a22acadffa67a2d
ilonatommy <32700855+ilonatommy@users.noreply.github.com>
2017-10-25 10:56:15 +0200

    Update Date.hpp

--- Date.hpp
+++ Date.hpp
@@ -26,6 +26,7 @@ public:
 	friend std::ostream& operator <<(std::ostream& os, const Date&);
 	friend int dateToDays(const Date& date);
 	friend Date& daysToDate(int days, Date& date);
+	friend virtual DateFormatter::std::string format(const Date& d) const;
 	
 	friend Date operator +(int& n, const Date& D);	//friend aby moÂżliwe byÂły operacje n+D i D+n
 	Date operator +(const Date& D); //D+D
