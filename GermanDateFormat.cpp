7dd3c061dfabd47b01e98e5dab31d1a7a927afb2
jagajda <32704908+jagajda@users.noreply.github.com>
2017-11-02 21:18:42 +0100

    Update GermanDateFormat.cpp
    teraz nasz fomratterz zwraca string, wcześniej zwracał strumień bufora

--- GermanDateFormat.cpp
+++ GermanDateFormat.cpp
@@ -1,9 +1,9 @@
 virtual std::string format(const Date& d) const override
  {
     std::ostringstream date_ (std::ostringstream::ate);
-    date_<< std::setfill('0') << std::setw(2) << Date->day<<".";
-    date_<< std::setfill('0') << std::setw(2) << Date->month << ".";
-    date_<< Date->year;    
-    
-    return date_;
+    date_<< std::setfill('0') << std::setw(2) << d.day<<".";
+    date_<< std::setfill('0') << std::setw(2) << d.month << ".";
+    date_<< d.year;   
+    std::string str=date_.str();
+    return str
 }
