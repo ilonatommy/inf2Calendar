cc7bc78edc45efd0ef091337cff8d4d9ff5291bd
jagajda <32704908+jagajda@users.noreply.github.com>
2017-11-02 21:21:29 +0100

    Update GermanDateFormat.cpp
    używa getterów, aby uzyskać dostęp do składowych prywatnych obiektu klasy Date

--- GermanDateFormat.cpp
+++ GermanDateFormat.cpp
@@ -1,9 +1,9 @@
 virtual std::string format(const Date& d) const override
  {
     std::ostringstream date_ (std::ostringstream::ate);
-    date_<< std::setfill('0') << std::setw(2) << d.day<<".";
-    date_<< std::setfill('0') << std::setw(2) << d.month << ".";
-    date_<< d.year;   
+    date_<< std::setfill('0') << std::setw(2) << d.getDay<<".";
+    date_<< std::setfill('0') << std::setw(2) << d.getMonth << ".";
+    date_<< d.getYear;   
     std::string str=date_.str();
     return str
 }
