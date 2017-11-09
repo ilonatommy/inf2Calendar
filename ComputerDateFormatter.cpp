a12a8cc1f1cbc49533c10876499801071f7f2b53
iski13 <32782549+iski13@users.noreply.github.com>
2017-11-09 13:41:40 +0100

    Delete ComputerDateFormatter.cpp

--- ComputerDateFormatter.cpp
+++
@@ -1,9 +0,0 @@
- virtual std::string format(const Date& d) const override
- {
-    std::ostringstream date_ (std::ostringstream::ate);
-    date_<< d.getYear << "-";
-    date_<< std::setfill('0') << std::setw(2) << d.getMonth << "-";
-    date_<< std::setfill('0') << std::setw(2) << d.getDay;
-    std::string str=date_.str();
-    return str;
- }
