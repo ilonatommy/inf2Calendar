01a66198fe288897b2b0f3ca2b6698c90b88e67d
iski13 <32782549+iski13@users.noreply.github.com>
2017-11-09 21:55:37 +0100

    Delete GermanDateFormat.cpp

--- GermanDateFormat.cpp
+++
@@ -1,9 +0,0 @@
-virtual std::string format(const Date& d) const override
- {
-    std::ostringstream date_ (std::ostringstream::ate);
-    date_<< std::setfill('0') << std::setw(2) << d.getDay<<".";
-    date_<< std::setfill('0') << std::setw(2) << d.getMonth << ".";
-    date_<< d.getYear;   
-    std::string str=date_.str();
-    return str
-}
