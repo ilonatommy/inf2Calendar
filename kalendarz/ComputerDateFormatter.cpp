cf25020faaed6072f7b3b6c12d4e15da849eb474
ilonatommy <ili.ili@interia.pl>
2017-11-25 10:09:20 +0100

    porządkowanie

---
+++ kalendarz/ComputerDateFormatter.cpp
@@ -0,0 +1,16 @@
+/*
+ * ComputerDateFormatter.cpp
+ *
+ *  Created on: 2 lis 2017
+ *      Author: adamg
+ */
+#include "ComputerDateFormatter.hpp"
+ std::string ComputerDateFormatter::format(const Date d) const
+ {
+    std::ostringstream date_;
+    date_<< d.getYear() << "-";
+    date_<< std::setfill('0') << std::setw(2) << d.getMonth() << "-";
+    date_<< std::setfill('0') << std::setw(2) << d.getDay();
+    std::string str=date_.str();
+    return str;
+}
---
+++ kalendarz/ComputerDateFormatter.hpp
@@ -0,0 +1,17 @@
+/*
+ * ComputerDateFormatter.hpp
+ *
+ *  Created on: 2 lis 2017
+ *      Author: adamg
+ */
+
+#ifndef COMPUTERDATEFORMATTER_HPP_
+#define COMPUTERDATEFORMATTER_HPP_
+#include "DateFormatter.hpp"
+#include "Date.hpp"
+class ComputerDateFormatter: public DateFormatter{
+public:
+    virtual std::string format(const Date d) const;
+};
+
+#endif /* COMPUTERDATEFORMATTER_HPP_ */
