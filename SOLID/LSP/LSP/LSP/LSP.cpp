96320be7af85c922d33e5b1a684c516967919c2c
ilonatommy <32700855+ilonatommy@users.noreply.github.com>
2017-11-25 15:34:25 +0100

    Update LSP.cpp

--- SOLID/LSP/LSP/LSP/LSP.cpp
+++ SOLID/LSP/LSP/LSP/LSP.cpp
@@ -1,12 +1,14 @@
 // LSP.cpp: Definiuje punkt wejścia dla aplikacji konsolowej.
 //WYJAŚNIENIE:
 /* 
-funkcja doSomething traktuje tak samo prostokat i kwadrat, przy czym settery w kwadracie nie pozwalajć na przypisanie róźnych wartości do
-wysokości i szerokości, więc ostatni setter ustawia wszystkie boki na tą samą wartość. Kwadrat nie potrzebuje aż dwóch getterów i setterów,
-tak samo nie potrzebuje dwóch wartości opisujących jego wielkość. Kod jest poprawny matematycznie, ale uzytkownik używający go może napisać
-funkcję doSomething, która nie będzie działała zgodnie z jego oczekiwaniami, z powodu konstrukcji klas Square i Rectangle. Naprawa:
+funkcja doSomething traktuje tak samo prostokat i kwadrat, przy czym settery w kwadracie nie pozwalają funkcji do Something na przypisanie 
+róźnych wartości do wysokości i szerokości, więc ostatni setter ustawia wszystkie boki na tą samą wartość. Kwadrat nie potrzebuje aż 
+dwóch getterów i setterów, tak samo nie potrzebuje dwóch wartości opisujących jego wielkość. Kod jest poprawny matematycznie, 
+ale uzytkownik używający go może napisać funkcję doSomething, która nie będzie działała zgodnie z jego oczekiwaniami, z powodu konstrukcji 
+klas Square i Rectangle. Naprawa:
 1)pozbywamy się dziedziczenia między Rectangle a Square
-2)  dodać pole Rectangle do Square traktując Rectangle jako rozszerzenie Square lub zbudować interfejs IShape łączący klasy Rectangle i Square 
+2)dodajemy pole Rectangle do Square traktując Rectangle jako rozszerzenie Square (lub budujemy interfejs IShape łączący klasy Rectangle i 
+Square)
 */
 
 #include "stdafx.h"
