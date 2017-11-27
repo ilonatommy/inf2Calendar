b010bd31dc7352ace0d8eef99f45e6bc51ebd9cf
jagajda <32704908+jagajda@users.noreply.github.com>
2017-11-27 21:07:37 +0100

    Add files via upload
    pierwsza wersja OCP
    wersja nie builduje się poprawnie- kwestia do wyjaśnienia na środowych zajęciach

---
+++ SOLID/OCP/main.cpp
@@ -0,0 +1,56 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 27 lis 2017
+ *      Author: Jakub
+ */
+
+#include <iostream>
+#include <vector>
+#include <memory>
+
+const int PI=3.14159265358;
+class Shape{
+public:
+	virtual double area()=0;
+};
+
+class Square:public Shape{
+private:
+	double _side;
+public:
+	double area() override{
+		return _side*_side;
+	};
+};
+class Circle:public Shape{
+private:
+	double _radius;
+public:
+	double area() override{
+		return PI*_radius*_radius;
+	};
+};
+
+class AreaCalculator{
+public:
+	double calculateArea(const std::vector<const Shape*>& shapes){
+		double totalArea=0;
+		for(const auto shape_ptr:shapes){
+			totalArea+=Shape::area.(*shape_ptr);
+		}
+		return totalArea;
+	};
+};
+
+int main(){
+	std::vector<std::unique_ptr<Shape>> v;
+	v.push_back(std::make_unique<Circle>(1.0));
+	v.push_back(std::make_unique<Square>(1.0));
+
+	std::vector<const Shape*> v_raw;
+	for(const auto& ptr:v_raw){
+		v_raw.push_back(ptr.get());
+	}
+	std::cout<<"total area= "<<AreaCalculator::calculateArea(v_raw)<<std::endl;
+}
