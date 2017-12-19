a5a88bd0dae87e09426b34fd73f7a7730cd3eb9e
jagajda <32704908+jagajda@users.noreply.github.com>
2017-12-19 16:25:49 +0100

    Update Link.h

--- Symulacja sieci/network/network/network/Link.h
+++ Symulacja sieci/network/network/network/Link.h
@@ -1,8 +1,14 @@
 #pragma once
 class Link
 {
+private:
+	int nodeID
+	int destID
+	enum src_type
+	enum dest_type
+	Preferences probability
 public:
-	Link();
+	Link(Sender&, Receiver&);
 	~Link();
 };
 
