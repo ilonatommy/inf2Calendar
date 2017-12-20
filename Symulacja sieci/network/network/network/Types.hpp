968665060f42db8f4e5eeea3bfe8de72defdad2b
jagajda <32704908+jagajda@users.noreply.github.com>
2017-12-20 10:02:19 +0100

    Create Types.hpp

---
+++ Symulacja sieci/network/network/network/Types.hpp
@@ -0,0 +1,16 @@
+#ifndef TYPES_HPP
+#define TYPES_HPP
+typedef unsigned int Time;
+typedef unsigned int TimeOffset;
+
+
+enum class QueueType{
+LIFO,
+FIFO
+}
+
+enum class NodeType{
+Worker,
+Ramp
+}
+#endif
