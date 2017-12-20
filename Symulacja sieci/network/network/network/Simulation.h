1ae5e3a831fb4d6d2c18aa6d41be21c69ee3d431
ilonatommy <32700855+ilonatommy@users.noreply.github.com>
2017-12-20 10:09:43 +0100

    Update Simulation.h

--- Symulacja sieci/network/network/network/Simulation.h
+++ Symulacja sieci/network/network/network/Simulation.h
@@ -4,5 +4,24 @@ class Simulation
 public:
 	Simulation();
 	~Simulation();
+	
+	bool isNetCorrect();
+	
+	std::vector<Ramp> getRamps();
+	std::vector<Storehouse> getStorehouses();
+	std::vector<Worker> getWorkers();
+	
+	void addRamp(Ramp);
+	void addStorehouse(Storehouse);
+	void addWorker(Worker);
+	
+	void deleteRamp(int ID); //odwołanie do this w funkcji
+	void deleteStorehouse(int ID);
+	void deleteWorker(int ID);
+	
+private:
+	std::vector<Ramp> ramps;
+	std::vector<Storehouse> storehouses;
+	std::vector<Worker> workers;	
 };
 
