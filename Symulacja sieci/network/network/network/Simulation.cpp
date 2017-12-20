ff7103006fdad589249f6cb4f1c4c24c1c40a894
ilonatommy <32700855+ilonatommy@users.noreply.github.com>
2017-12-20 10:37:56 +0100

    Update Simulation.cpp

--- Symulacja sieci/network/network/network/Simulation.cpp
+++ Symulacja sieci/network/network/network/Simulation.cpp
@@ -2,11 +2,44 @@
 #include "Simulation.h"
 
 
-Simulation::Simulation()
+Simulation::Simulation(std::ostringstream & stream)
 {
 }
 
-
 Simulation::~Simulation()
+{
+}
+
+bool isNetCorrect()
+{
+}
+	
+std::vector<Ramp> getRamps()
+{
+}
+std::vector<Storehouse> getStorehouses()
+{
+}
+std::vector<Worker> getWorkers()
+{
+}
+	
+void addRamp(Ramp)
+{
+}
+void addStorehouse(Storehouse)
+{
+}
+void addWorker(Worker)
+{
+}
+
+void deleteRamp(int ID)
+{
+}
+void deleteStorehouse(int ID)
+{
+}
+void deleteWorker(int ID)
 {
 }
