e4508e54ff3c9fd81ef1c7d66f66660325e0e8c0
ilonatommy <32700855+ilonatommy@users.noreply.github.com>
2018-01-03 10:35:24 +0100

    Update Worker.cpp

--- Symulacja sieci/network/network/network/Worker.cpp
+++ Symulacja sieci/network/network/network/Worker.cpp
@@ -2,15 +2,29 @@
 #include "Worker.h"
 
 
-Worker::Worker(ID _id, TimeOffset _processingDuration, Time _processingStartTime,QueueType* _queueType)
+Worker::Worker(ID _id, TimeOffset _processingDuration, Time _processingStartTime,IQueue* _queue)
 {
   	(*this).processingStartTime = _processingStartTime; 
 	(*this).processingDuration = _processingDuration; 
 	(*this).id = _id;
-  	(*this).queueType = _queueType;
+  	(*this).queue = _queue;
 }
 
 
 Worker::~Worker()
 {
+}
+
+void receive_product(Product)
+{
+	(*this).queue.push(Product);		
+}
+
+void displayQueue()
+{
+	while (!(*this).queue.empty())
+  {
+    	std::cout << q.front() << " ";
+    	(*this).queue.pop();
+  }
 }
