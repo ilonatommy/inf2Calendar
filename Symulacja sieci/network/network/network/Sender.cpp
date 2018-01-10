ce2d10888fb76acf6ecbd90c3a720d9bd2b436cb
ilonatommy <32700855+ilonatommy@users.noreply.github.com>
2018-01-10 10:57:09 +0100

    Update Sender.cpp

--- Symulacja sieci/network/network/network/Sender.cpp
+++ Symulacja sieci/network/network/network/Sender.cpp
@@ -11,3 +11,23 @@ Sender::Sender()
 Sender::~Sender()
 {
 }
+
+Preferences getReceiverPreferences()
+{
+  return (*this).receiverPreferences;
+}
+
+setReceiverPreferences(Preferences)
+{
+  //czytanie z pliku? Po co skoro już to robimy w konstruktorze?
+}
+
+void sendProduct(Receiver receiver,Product product)
+{
+  receiver.ReceivePackage(&product);
+}
+
+std::vector<Product> getSendingBuffer()
+{
+  return (*this).sendingBuffer;
+}
