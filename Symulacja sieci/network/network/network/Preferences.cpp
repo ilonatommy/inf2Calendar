bcec49242241e0a37eb0b143d1c87633712a04c4
jagajda <32704908+jagajda@users.noreply.github.com>
2018-01-10 10:56:11 +0100

    Update Preferences.cpp

--- Symulacja sieci/network/network/network/Preferences.cpp
+++ Symulacja sieci/network/network/network/Preferences.cpp
@@ -4,9 +4,25 @@
 
 Preferences::Preferences()
 {
+  probabilities=std::unordered_map<Receiver*,double> map();
 }
 
-
-Preferences::~Preferences()
+Preferences::std::unordered_map<Receiver* ,double> getPreferences()
 {
+  return (*this).probabilities;
+}
+
+void Preferences::setProbabilities(std::unordered_map<Receiver* , double> map)
+{
+  (*this).probabilities=map;
+}
+
+void addReceiver(Receiver* r) {
+  for(auto& entry : probabilities) {
+    
+  for(auto it = probabilities.begin(); it != probabilities.end(); it++) {
+    auto& entry = *it;
+    // modyfikacja prawdop.
+    entry.second
+  }
 }
