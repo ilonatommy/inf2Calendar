ae27070c2ad20661972a5f7f0a5b5af22065cb43
jagajda <32704908+jagajda@users.noreply.github.com>
2018-01-10 10:36:48 +0100

    Update Storehouse.cpp

--- Symulacja sieci/network/network/network/Storehouse.cpp
+++ Symulacja sieci/network/network/network/Storehouse.cpp
@@ -2,12 +2,24 @@
 #include "Storehouse.h"
 
 
-Storehouse::Storehouse()
+Storehouse::Storehouse(ID =_id)
 {
- 
+ list_of_product=std::vector<Product> vec;
+ elemID=_id;
 }
 
-
-Storehouse::~Storehouse()
+void::Storehouse receive_package(Package)
 {
+  (*this).list_of_product.push-back(Package);
 }
+
+ID Storehouse::getID(){return (*this).elemenID}
+
+void Storehouse::view()
+{
+ for(auto i:(*this).list_of_product)
+ {
+   std::cout<<i<<std::endl;
+ }
+}
+
